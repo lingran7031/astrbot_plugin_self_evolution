4f5bbb217f05d9995db92e52275683c55c1771e3
Renyus <39555912+Renyus@users.noreply.github.com>
2026-03-28 19:18:32 +0800

    up to 3.3

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class PromptContext:
     "astrbot_plugin_self_evolution",
     "自我进化 (Self-Evolution)",
     "CognitionCore 7.0 数字生命。",
-    "Ver 3.2.2",
+    "Ver 3.3",
 )
 class SelfEvolutionPlugin(Star):
     @staticmethod
